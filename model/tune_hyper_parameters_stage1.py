b2581f1a34279bec3c1344035d5ceb644e03cac0
Gleb Milyukov <cvresearch.milyukov@gmail.com>
2023-09-27 22:03:24 +0400

    Modifed script for hyper-parameters tuning to log metrics to Tensorboard

--- model/tune_hyper_parameters_stage1.py
+++ model/tune_hyper_parameters_stage1.py
@@ -9,6 +9,9 @@ from tensorboard.plugins.hparams import api as hp
 
 import tensorflow as tf
 
+METRIC_LOSS = 'loss'
+METRIC_EPOCH_LOSS = 'epoch_loss'
+
 # tune HP
 def run(queue, run_dir, hparams):
     config = tf.compat.v1.ConfigProto()
@@ -17,43 +20,64 @@ def run(queue, run_dir, hparams):
     session = tf.compat.v1.Session(config=config)
 
     label_encoder = LabelEncoder()
-    batch_size = 1
-    train_dataset, dataset_info = tfds.load(
-        "meter_values_dataset_stage1", split=["train"], with_info=True,
+    batch_size = 8
+    autotune = tf.data.AUTOTUNE
+    (train_dataset, val_dataset, test_dataset), dataset_info = tfds.load(
+        "meter_values_dataset_stage1", split=["train", "val", "test"], with_info=True,
         read_config=tfds.ReadConfig(try_autocache=False)
     )
     train_dataset = train_dataset[0]
-    train_dataset = train_dataset.map(preprocess_data, num_parallel_calls=1)#autotune)
+    train_dataset = train_dataset.map(preprocess_data, num_parallel_calls=autotune)
+    train_dataset = train_dataset.shuffle(batch_size)
     train_dataset = train_dataset.padded_batch(
         batch_size=batch_size, padding_values=(0.0, 1e-8, -1), drop_remainder=True
     )
     train_dataset = train_dataset.map(
-        label_encoder.encode_batch, num_parallel_calls=1#autotune
+        label_encoder.encode_batch, num_parallel_calls=autotune
     )
     train_dataset = train_dataset.apply(tf.data.experimental.ignore_errors())
-    
-    num_classes = 4
-    resnet50_backbone = get_backbone()
-    loss_fn = RetinaNetLoss(num_classes)
-    model = RetinaNet(num_classes, resnet50_backbone)
-    
-    optimizer = tf.keras.optimizers.Adam(learning_rate=hparams['learning_rate'])
-    model.compile(loss=loss_fn, optimizer=optimizer)#, run_eagerly=True)
+    train_dataset = train_dataset.prefetch(autotune)
 
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=run_dir, histogram_freq=1)
-    terminate_on_nan = tf.keras.callbacks.TerminateOnNaN()
-    
-    epochs = 30
+    val_dataset = val_dataset.map(preprocess_data, num_parallel_calls=autotune)
+    val_dataset = val_dataset.padded_batch(
+        batch_size=1, padding_values=(0.0, 1e-8, -1), drop_remainder=True
+    )
+    val_dataset = val_dataset.map(label_encoder.encode_batch, num_parallel_calls=autotune)
+    val_dataset = val_dataset.apply(tf.data.experimental.ignore_errors())
+    val_dataset = val_dataset.prefetch(autotune)
+
+    test_dataset = test_dataset.map(preprocess_data, num_parallel_calls=autotune)
+    test_dataset = test_dataset.padded_batch(
+        batch_size=1, padding_values=(0.0, 1e-8, -1), drop_remainder=True
+    )
+    test_dataset = test_dataset.map(label_encoder.encode_batch, num_parallel_calls=autotune)
+    test_dataset = test_dataset.apply(tf.data.experimental.ignore_errors())
+    test_dataset = test_dataset.prefetch(autotune)
+
     with tf.summary.create_file_writer(run_dir).as_default():
+        hp.hparams(hparams)  # record the values used in this trial
+        num_classes = 4
+        resnet50_backbone = get_backbone()
+        loss_fn = RetinaNetLoss(num_classes)
+        model = RetinaNet(num_classes, resnet50_backbone)
+        
+        optimizer = tf.keras.optimizers.Adam(learning_rate=hparams['learning_rate'])
+        model.compile(loss=loss_fn, optimizer=optimizer, run_eagerly=True)
+
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=run_dir, histogram_freq=1)
+        terminate_on_nan = tf.keras.callbacks.TerminateOnNaN()
+        
+        epochs = 5
         model.fit(
-            train_dataset.take(1),
+            train_dataset,
+            validation_data=val_dataset,
             epochs=epochs,
             callbacks=[tensorboard_callback, terminate_on_nan],
             verbose=1,
         )
-        loss = model.evaluate(train_dataset.take(1))
+        loss = model.evaluate(test_dataset)
         queue.put(loss)
-        tf.summary.scalar('Loss', loss, step=1)
+        tf.summary.scalar(METRIC_LOSS, loss, step=1)
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn')
@@ -62,9 +86,18 @@ if __name__ == '__main__':
     print(f"tensorflow version: {tf.__version__}")
 
     log_dir = "logs/hparam_tuning/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    
     HP_LR = hp.HParam(
         'learning_rate', 
-        hp.Discrete([0.1 ** i for i in range(6)])
+        hp.Discrete([0.1 ** i for i in range(2, 6)])
+        )
+    
+    with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
+        hp.hparams_config(
+            hparams=[HP_LR],
+            metrics=[hp.Metric(METRIC_LOSS, display_name='Loss'),
+                     hp.Metric(METRIC_EPOCH_LOSS, display_name='Batch loss')],
         )
 
     session_num = 0
