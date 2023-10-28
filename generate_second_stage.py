04b3208729e7498dd3070765c05512154c49ac50
Gleb Milyukov <cvresearch.milyukov@gmail.com>
2023-10-28 07:43:49 +0000

    Improved script for stage 2 images generation

--- generate_second_stage.py
+++ generate_second_stage.py
@@ -4,6 +4,7 @@ import cv2
 import sys
 import requests
 import numpy as np
+import tqdm
 
 # Get the parent directory
 parent_dir = os.path.dirname(os.path.realpath(__file__)) + '/model/'
@@ -15,14 +16,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("images", help="path to images", type=str)
 parser.add_argument("output", help="path to cropped images", type=str)
 
-ENDPOINT_URL = "http://localhost:8000/recognize"
+ENDPOINT_URL = "http://localhost:8080/recognize"
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    for filename in os.listdir(args.images):
-        if not filename.lower().endswith(('.jpg', '*.png', '*.bmp')):
+    files = os.listdir(args.images)
+    print(f'Number of files in a folder: {len(files)}') 
+
+    for index, filename in enumerate(tqdm.tqdm(os.listdir(args.images))):
+        if not filename.lower().endswith(('.jpg', '.jpeg', '*.png', '*.bmp')):
             continue
+        if os.path.exists(os.path.join(args.output, 'cropped_' + filename)):
+            print(os.path.join(args.output, 'cropped_' + filename) + ' exists')
+            continue
+        print(f"Generating: {os.path.join(args.output, 'cropped_' + filename)}")
         image = cv2.imread(os.path.join(args.images, filename))
         ratio = 1333.0 / max(image.shape[:2])
         image = cv2.resize(image, (0, 0), fx = ratio, fy = ratio, interpolation=cv2.INTER_CUBIC)
@@ -34,9 +42,5 @@ if __name__ == '__main__':
         response.raise_for_status()
         response = response.json()
         if response['success']:
-            boxes = np.array(response["bboxes"])
-            scores = np.array(response["scores"])
-            class_names = np.array(response["class_names"])
-            ratio = response["ratio"]
             image_cropped = np.array(response["image_cropped"])
-            cv2.imwrite(os.path.join(args.output, 'cropped_' + filename))
+            cv2.imwrite(os.path.join(args.output, 'cropped_' + filename), image_cropped)
